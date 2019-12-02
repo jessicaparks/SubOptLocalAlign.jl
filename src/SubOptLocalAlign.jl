d8ded8127694abaea257ebd5204cc55f8aa4b618
jessicaparks <jparks@agbiome.com>
2019-12-02 18:51:54 -0500

    add docstring and align code

--- src/SubOptLocalAlign.jl
+++ src/SubOptLocalAlign.jl
@@ -1,8 +1,12 @@
 __precompile__()
 
+"""
+# SubOptLocalAlign
+Identify and visualize sub-optimal local alignments.
+"""
 module SubOptLocalAlign
 
-
 include("read_inputs.jl")
+include("align.jl")
 
 end
