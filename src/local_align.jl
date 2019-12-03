79ba43295fa72aa9039598ae5ef85c1c32888e05
jessicaparks <jparks@agbiome.com>
2019-12-03 11:41:34 -0500

    update docstring

--- src/local_align.jl
+++ src/local_align.jl
@@ -6,29 +6,51 @@
 
 
 """
-traceback(align_pos, trace_matrix, score_matrix, seqA, seqB, alignments, scores, positions,
-          thresh, dedup, current_alignment, current_scores, current_positions, maxpathscore)
-traceback to identify alignments
-
-Arguments:
-align_pos: coordinates of the alignment end position
-trace_matrix: alignment traceback matrix
-score_matrix: score matrix for the alignment
-seqA: sequence A
-seqB: sequence B
-alignments: empty list to fill with resulting alignments
-scores: empty list to fill with resulting scores
-positions: empty list to fill with resulting positions
-current_alignment: sequences in the current alignment at the position
-current_scores: scores at each position along the current alignment
-current_positions: coordinates of each position in the current alignment
-
-Returns:
-pushes updates to the alignments, scores, and positions arrays
+    traceback(align_pos::CartesianIndex{2},
+        trace_matrix::Array{Array{Int,1},2}, score_matrix::Array{Int,2},
+        seqA::String, seqB::String,
+        alignments::Array{Tuple{String,String},1},
+        scores::Array{Array{Int,1},1},
+        positions::Array{Array{Tuple{Int,Int},1},1})
+    traceback(; thresh::Int=0, dedup::Bool=true,
+        current_alignment::Tuple{String,String}=("",""),
+        current_scores::Array{Int,1}=[],
+        current_positions::Array{Tuple{Int,Int},1}=[],
+        maxpathscore::Union{Int,Bool}=false)
+  
+Trace path back through matrix to identify alignments. Push output to `alignments`,
+`scores`, and `positions`.  
+  
+---
+# Arguments:
+- `align_pos::CartesianIndex{2}`: coordinates of the alignment end position, and then
+    each current position as the traceback continues.
+- `trace_matrix::Array{Array{Int,1},2}`: alignment traceback matrix.
+- `score_matrix::Array{Int,2}`: score matrix for the alignment.
+- `seqA::String`: first input sequence.
+- `seqB::String`: second input sequence.
+- `alignments::Array{Tuple{String,String},1}`: populate with output alignments.
+- `scores::Array{Array{Int,1},1}`: populate with output scores.
+- `positions::Array{Array{Tuple{Int,Int},1},1}`: populate with output positions.
+- `thresh::Int=0`: score threshold for the alignment.
+- `dedup::Bool=true`: whether alignments should be deduplicated.
+- `current_alignment::Tuple{String,String}=("","")`: sequences of the current alignment.
+- `current_scores::Array{Int,1}=[]`: scores at each position along the current alignment.
+- `current_positions::Array{Tuple{Int,Int},1}=[]`: coordinates of each position in the
+    current alignment.
+- `maxpathscore::Union{Int,Bool}=false`: the maximum score along the traceback path,
+    starts at `false`.
 """
-function traceback(align_pos, trace_matrix, score_matrix, seqA, seqB, alignments, scores, positions;
-                   thresh=0, dedup=true,
-                   current_alignment=("",""), current_scores=[], current_positions=[], maxpathscore=false)
+function traceback(align_pos::CartesianIndex{2}, trace_matrix::Array{Array{Int,1},2},
+                   score_matrix::Array{Int,2}, seqA::String, seqB::String,
+                   alignments::Array{Tuple{String,String},1}, scores::Array{Array{Int,1},1},
+                   positions::Array{Array{Tuple{Int,Int},1},1};
+                   thresh::Int=0, dedup::Bool=true,
+                   current_alignment::Tuple{String,String}=("",""),
+                   current_scores::Array{Int,1}=[],
+                   current_positions::Array{Tuple{Int,Int},1}=[],
+                   maxpathscore::Union{Int,Bool}=false)
+
     (a, b) = Tuple(align_pos)
     trace = trace_matrix[a,b]
     score = score_matrix[a,b]
@@ -77,31 +99,33 @@ end
 
 
 """
-deduplicate(align_pos, trace_matrix, score_matrix, thresh, lenA, lenB)
-identify whether the alignment end position has any parents in the trace matrix
-where a parent is a next alignment position that would contain this position in its alignment
-
-Arguments:
-align_pos: coordinates of the alignment position
-trace_matrix: alignment traceback matrix
-score_matrix: score matrix for the alignment
-thresh: alignment threshold
-lenA: length of seqA + 1 (the max size of the alignment in the a direction)
-lenB: length of seqB + 1 (the max size of the alignment in the b direction)
-
-Returns:
-(bool): true if the alignment position is contained within a parent alignment, false if not;
+    deduplicate(align_pos::CartesianIndex{2}, trace_matrix::Array{Array{Int,1},2},
+        score_matrix::Array{Int,2}, thresh::Int, lenA::Int, lenB::Int)
+  
+Identify whether the alignment end position has any parents in the trace matrix where a
+parent is a next alignment position that would contain this position in its alignment.
+Return `true` if the alignment position is contained within a parent alignment, and
+`false` if not.  
+  
+---
+# Arguments:
+- `align_pos::CartesianIndex{2}`: coordinates of the alignment position.
+- `trace_matrix::Array{Array{Int,1},2}`: alignment traceback matrix.
+- `score_matrix::Array{Int,2}`: score matrix for the alignment.
+- `thresh::Int`: alignment score threshold.
+- `lenA::Int`: length of seqA + 1 (the max size of the alignment in the a direction).
+- `lenB::Int`: length of seqB + 1 (the max size of the alignment in the b direction).
 """
-function deduplicate(align_pos, trace_matrix, score_matrix, thresh, lenA, lenB)
+function deduplicate(align_pos::CartesianIndex{2}, trace_matrix::Array{Array{Int,1},2},
+                     score_matrix::Array{Int,2}, thresh::Int, lenA::Int, lenB::Int)
+
     (a, b) = Tuple(align_pos)
     if a==lenA && b==lenB
         return false
     elseif a==lenA
-        # could remove the recursion because only adding gaps
         return ((4 in trace_matrix[a,b+1]) && ((score_matrix[a,b+1]>=thresh) || 
                                               DeDup((a,b+1), trace_matrix, score_matrix, thresh, lenA, lenB)))
     elseif b==lenB
-        # could remove the recursion because only adding gaps
         return ((3 in trace_matrix[a+1,b]) && ((score_matrix[a+1,b]>=thresh) || 
                                               DeDup((a+1,b), trace_matrix, score_matrix, thresh, lenA, lenB)))
     else
@@ -116,27 +140,33 @@ end
 
 
 """
-local_align(seqA, seqB, sub_header, sub_matrix, thresh, gap_open, gap_extend, dedup, dedup_method)
-identify all suboptimal alignments of SeqA and SeqB scoring above threshhold
-
-Arguments:
-seqA: first input sequence
-seqB: second input sequence
-thresh (int): threshold for the alignment score, defaults to false (which triggers an automatic threshold)
-sub_header (list): substitution matrix headers
-sub_matrix (matrix): substitution matrix
-gap_open (int): contribution to alignment score for each gap opening, default = -12
-gap_extend (int): contribution to alignment score for each gap extension, default = -4
-dedup (bool): filter "duplicate" alignments (on the same path) if true, do not filter if false
-dedup_method (str): method for filtering, either longest or highest-scoring alignment ("length" or "score")
-
-Returns:
-score matrix
-traceback matrix
-(start position, end position, score, alignment) list for all suboptimal alignments
+    local_align(seqA::String, seqB::String,
+        sub_header::Dict{Char,Int}, sub_matrix::Array{Float,2})
+    local_align(; thresh::Union{Int,Bool}=false, gap_open::Int=-12, gap_extend::Int=-4,
+        dedup::Bool=true, dedup_method::String="score")
+  
+Identify suboptimal alignments of SeqA and SeqB scoring above threshhold. Return the
+`score matrix`, `trace matrix`, and an array of alignments, containing `start coordinate`,
+`end coordinate`, `score`, and `aligned sequences` for each.  
+  
+---
+# Arguments:
+- `seqA::String`: first input sequence.
+- `seqB::String`: second input sequence.
+- `sub_header::Dict{Char,Int}`: substitution matrix headers.
+- `sub_matrix::Array{Float,2}`: substitution matrix.
+- `thresh::Union{Int,Bool}=false`: threshold for the alignment score, `false` triggers an
+    automatically-set threshold.
+- `gap_open::Int=-12`: penalty for gap opening.
+- `gap_extend::Int=-4`: penalty for gap extension.
+- `dedup::Bool=true`: whether to filter duplicate alignments (on the same path).
+- `dedup_method::String="score"`: method for deduplicating, either longest or highest-scoring
+    alignment ("length" or "score").
 """
-function local_align(seqA, seqB, sub_header, sub_matrix;
-                    thresh=false, gap_open=-12, gap_extend=-4, dedup=true, dedup_method="score")
+function local_align(seqA::String, seqB::String,
+                     sub_header::Dict{Char,Int}, sub_matrix::Array{Float,2};
+                     thresh::Union{Int,Bool}=false, gap_open::Int=-12, gap_extend::Int=-4,
+                     dedup::Bool=true, dedup_method::String="score")
     
     # instantiate scoring matrix and trace matrix
     score_matrix = fill(0,(length(seqA)+1,length(seqB)+1))
